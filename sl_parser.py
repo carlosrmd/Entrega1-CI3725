ea83a5a72dc14adf0b3e9fcc8e0454efbc7e181d
christian <chris.atg.2x@gmail.com>
2015-02-07 20:41:26 -0430

    Modified sl_parser.py

---
+++ sl_parser.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#	TRADUCTORES E INTERPRETADORES CI3725      #
+#	Primera entrega del proyecto.             #
+#   Lexer para el lenguaje Setlan             #
+#	Autores: Carlos Martínez 	- 11-10584    #
+#			 Christian Teixeira - 11-10584    #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+import ply.yacc as yacc
+from sl_lexer import tokens
+
