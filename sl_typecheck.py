15796e3b996638e07bef1307ea71fd698ad8573a
Carlos <carlosrmd94@gmail.com>
2015-02-23 18:16:57 -0430

    More error cases added

--- sl_typecheck.py
+++ sl_typecheck.py
@@ -16,6 +16,7 @@ from sl_symtab import SymTab, ST_Stack
 # "nodec": Uso de variable no declarada
 # "inv_tex": Tipo de expresion invalido
 # "inv_opr": Operacion o tipo de operandos invalidos
+# "inv_assign" Asignación inválida
 # "read_ol": Asignacion a una variable read_only (de un FOR)
 
 error_st = []
@@ -73,7 +74,7 @@ def build_symbol_table_REC(AST):
 		if aux:
 			error_st.append(("redec", name, lin_dec, col_dec))
 		st_stack.top().insert(name, dec_scope, type, val, lin_dec)
-		print(st_stack.stack[0].typeof("n", 1))
+		print(st_stack)
 
 		strrep_st = strrep_st + "\t"*indent_level + str(st_stack.top().var_str(name, dec_scope)) + "\n"
 
@@ -114,6 +115,16 @@ def build_symbol_table_REC(AST):
 			if not declared:
 				error_st.append(("nodec", name, lin_dec, col_dec))
 
+		varname = AST.children[0].children[0].val
+		for i in range(num_scopes):
+			if st_stack.stack[0].typeof(varname, i+1):
+				to_assign_type = st_stack.stack[0].typeof(varname, i+1)
+		print("Buey Almizclero " + to_assign_type)
+		assign_value_type = gettype(AST.children[1].children[0])
+		if assign_value_type != "error" and assign_value_type != to_assign_type:
+			error_st.append(("inv_assign", "=" , AST.lineno, AST.colno, to_assign_type, assign_value_type))
+
+
 	# Tipos AST de asignacion de variables y evaluacion de expresiones
 	# Realiza la funcion correspondiente al tipo de nodo (AST.type)
 
@@ -131,6 +142,54 @@ def build_symbol_table(AST):
 	build_symbol_table_REC(AST)
 	if len(error_st) == 0: return strrep_st
 	else: return None
+
+def gettype(AST):
+
+	if AST.val == "int" or AST.val == "set":
+		print("Zarramagurdi " + AST.type)
+		return AST.val
+	if AST.val == "true" or AST.val == "false":
+		return "bool"
+	if AST.val == "constant":
+		return gettype(AST.children[0])
+	if AST.val == "variable":
+		for i in range(num_scopes):
+			if st_stack.stack[0].typeof(AST.children[0].val, i+1):
+				to_assign_type = st_stack.stack[0].typeof(AST.children[0].val, i+1)
+		return to_assign_type
+
+
+	if AST.type == "expr_binopr":
+		a = gettype(AST.children[0])
+		b = gettype(AST.children[1])
+		if a == b and a == "int":
+			return a
+		else:
+			if a != "error" and b != "error": error_st.append(("inv_opr", AST.val[-1:] , AST.lineno, AST.colno, a, b))
+			return "error"
+	if AST.type == "expr_cmpopr":
+		a = gettype(AST.children[0])
+		b = gettype(AST.children[1])
+		if a == b and a == "int":
+			return "bool"
+		else:
+			opr = AST.val.split(" ")
+			if a != "error" and b != "error": error_st.append(("inv_opr", opr[1] , AST.lineno, AST.colno, a, b))
+			return "error"
+	if AST.type == "expr_setcont":
+		a = gettype(AST.children[0])
+		b = gettype(AST.children[1])
+		if a == "int" and b == "set":
+			return "bool"
+		else:
+			if a != "error" and b != "error": error_st.append(("inv_opr", "@" , AST.lineno, AST.colno, a, b))
+			return "error"
+
+	if AST.type == "negate_stmt":
+		if gettype(AST.children[0]) == "int":
+			return "int"
+		else:
+			
 
 def getvar_list(AST):
 	var_string = getvars(AST)
@@ -154,11 +213,13 @@ def getvars(AST):
 	elif AST.val == "set":
 		toreturn = ""
 		for child in AST.children:
-			print("Si no te gusta como pienso mamame el guevo " + child.val)
 			toreturn = toreturn + "+" + getvars(child)
 		return toreturn
 	else:
+		print(AST.val)
 		uno = getvars(AST.children[0])
+		if len(AST.children) == 1:
+			return uno
 		dos = getvars(AST.children[1])
 		if dos == "":
 			return uno
