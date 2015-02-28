e57c718feade69f1b17314257125e3af9f9ebc0d
christian <chris.atg.2x@gmail.com>
2015-02-28 09:27:40 -0430

    Interpreter module.

---
+++ sl_interpreter.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#	TRADUCTORES E INTERPRETADORES CI3725      #
+#	Cuarta entrega del proyecto.              #
+#   Interpretador para el lenguaje Setlan     #
+#	Autores: Carlos Martínez 	- 11-10584    #
+#			 Christian Teixeira - 11-11016    #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
